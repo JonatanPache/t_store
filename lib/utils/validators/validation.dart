bcbe9ddac423791d5d763f1a3e5e8edf6ea69118
JonatanPache <yonh.guitar777@gmail.com>
2024-02-02 17:57:13 -0400

    ok only splash flutter native

--- lib/utils/validators/validation.dart
+++ lib/utils/validators/validation.dart
@@ -5,5 +5,22 @@ class TValidator {
     if(value == null || value.isEmpty) {
       return 'Email is required';
     }
+    final emailRegExp = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    if(!emailRegExp.hasMatch(value)){
+      return 'Invalid email';
+    }
+
+    return null;
+  }
+
+  static String? validatePassword(String? value){
+    if (value == null || value.isEmpty) {
+      return 'Password is required.';
+    }
+
+    if (value.length < 6){
+      return 'Password must be at least 6 characters long.';
+    }
+    return null;
   }
 }
